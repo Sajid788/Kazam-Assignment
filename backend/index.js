c1700a67eebd9e35131a62ed1854a6780a1c7196
Sajid788 <mdsajidalam8340@gmail.com>
2025-02-06 17:19:34 +0530

    user registe and login done

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const { connection, PORT } = require("./config/db");
+const { userRouter } = require("./routes/UserRoutes");
 
 const app = express();
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
     res.json({ message: "Server is running!"});
 });
+
+
+app.use("/api/users", userRouter);
+
 
 app.listen(PORT, async () => {
   try {
